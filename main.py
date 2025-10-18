6e3284605da13f14788f894c82a0d8603a3a5706
eunseo <2023180012@tukorea.ac.kr>
2025-10-19 05:58:31 +0900

    클래스 분할

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 from pico2d import *
-import random
+
+from mage import Mage
 
 WIDTH, HEIGHT = 1000, 600
-
-class Mage:
-    def __init__(self, x=40, y=300):
-        self.x = x
-        self.y = y
-        self.image = load_image("player_sprite.png")
-        # (x, y) 좌표를 담는 프레임 리스트
-        self.frames = [0, 33, 66]
-        self.frame = 0
-
-    def update(self):
-        self.x += 1
-        self.frame = (self.frame + 1) % 3
-    def draw(self):
-        if self.image:
-            self.image.clip_draw(self.frames[self.frame], 0, 32, 40, self.x, 90)
-
 
 class Knight:
     pass
