0676fba4cf9b690606a6b8aaf578893fc277fa18
eunseo <2023180012@tukorea.ac.kr>
2025-10-19 04:48:09 +0900

    기본 루프 및 핸들 이벤트 함수 작성

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from pico2d import *
 
+def handle_events():
+    global running
+
+    event_list = get_events()
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
+running = True
+
+open_canvas()
+
+# game loop
+
+while running:
+    handle_events()
+    # close_canvas()
+
+close_canvas()
