e2046959c151648e0f9ffceefd49da4ed1b3a5ce
eunseo <2023180012@tukorea.ac.kr>
2025-10-31 17:04:36 +0900

    initial title_mode

---
+++ title_mode.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+image = None
+running = True
+
+def init():
+    pass
+
+def finish():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
+def handle_events():
+    pass
