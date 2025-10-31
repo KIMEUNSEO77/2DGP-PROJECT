101a74c49459adf300dfab31fd2cf53fbf5a5c2e
eunseo <2023180012@tukorea.ac.kr>
2025-10-31 17:10:37 +0900

    title_mode 띄우기

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from pico2d import *
 import play_mode
+import title_mode
 
 open_canvas(play_mode.WIDTH, play_mode.HEIGHT)
-play_mode.init()
+# play_mode.init()
+title_mode.init()
 
+'''
 while play_mode.running:
     play_mode.handle_events()
     play_mode.update()
@@ -16,4 +19,12 @@ while play_mode.running:
     play_mode.draw()
     delay(0.05)
 play_mode.finish()
+'''
+
+while title_mode.running:
+    title_mode.handle_events()
+    title_mode.update()
+    title_mode.draw()
+    delay(0.05)
+title_mode.finish()
 close_canvas()
--- title_mode.py
+++ title_mode.py
@@ -2,18 +2,28 @@ from pico2d import *
 
 image = None
 running = True
+title_start_time = 0.0
 
 def init():
-    pass
+    global image, running, title_start_time
+    image = load_image('title_scene.png')
+    running = True
+    title_start_time = get_time()
 
 def finish():
-    pass
+    global image
+    del image
 
 def update():
-    pass
+    global running, title_start_time
+    if get_time() - title_start_time > 2.0:
+        title_start_time = get_time()
+        running = False
 
 def draw():
-    pass
+    clear_canvas()
+    image.draw(500, 300)
+    update_canvas()
 
 def handle_events():
-    pass
+    events = get_events()   # 현재 이벤트들을 소비
